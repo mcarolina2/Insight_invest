476288cafc9d1278afefb4970399b8c93b1ef2ab
Carol <108403441+mcarolina2@users.noreply.github.com>
2026-02-06 04:30:44 -0300

    criando os models e fazendo a primeira migração para o banco de dados.

--- Insight_invest/settings.py
+++ Insight_invest/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users', 
+    'scoring',
+    'portfolio',
+    'ml_engine',
+    'market_data',
+    'interaction',
+    'analysis',
+    'content',
 ]
 
 MIDDLEWARE = [
